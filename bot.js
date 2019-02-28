906226a1806a18e31ac147a0aa6fa50f7d0145a9
Mythicbonzo0802 <conzept0802@gmail.com>
2019-02-28 17:21:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,21 +13,3 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
-// status
-client.user.setStatus('dnd')
-
-// game and streaming
-static void UpdatePresence()
-{
-    DiscordRichPresence discordPresence;
-    memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.state = "Surveilling";
-    discordPresence.details = "https://planetzeu.net/";
-    discordPresence.largeImageKey = "web";
-    discordPresence.largeImageText = "Planet Z EU";
-    discordPresence.smallImageKey = "ppb";
-    discordPresence.partyId = "nahh";
-    discordPresence.joinSecret = "nah";
-    Discord_UpdatePresence(&discordPresence);
-}
