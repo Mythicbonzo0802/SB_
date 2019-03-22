11d35dd115f385b1587c9a29130a9d879ffd03f4
Mythicbonzo0802 <conzept0802@gmail.com>
2019-03-22 17:06:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    bot.user.setGame('surveilling')
+    client.user.setGame('surveilling')
 });
 
 client.on('message', message => {
